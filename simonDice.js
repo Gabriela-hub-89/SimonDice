83dea78bd3e0d85366ef1d74470dc75d1080be4b
Gabriela <60906973+Gabriela-hub-89@users.noreply.github.com>
2022-08-02 16:07:52 -0300

    crea la funcion para capturar elemento random del juego

--- simonDice.js
+++ simonDice.js
@@ -1,67 +1,126 @@
 
 
-let ronda = 0
-let rondas = []
-let rondaJuego = []
-let rondaUsuario = []
+// let ronda = 0
+// let rondas = []
+// let rondaJuego = []
+// let rondaUsuario = []
 
 
-document.querySelector('#empezar').onclick = empezarJuego
-
-
-
-function empezarJuego() {
-
-    ronda++
+// document.querySelector('#empezar').onclick = empezarJuego
 
 
 
+// function empezarJuego() {
 
-    // numeroRandom()
-    resaltarCuadro(numeroRandom(), capturarCuadros())
+//     ronda++
+
+
+
+
+//     // numeroRandom()
+//     // resaltarCuadro(numeroRandom(), capturarCuadros())
+//     turnoJugador(resaltarCuadro())
     
-}
+// }
 
 
-function resaltarCuadro(numeroRandom, cuadros) {
+// function resaltarCuadro(numeroRandom, cuadros) {
+
+//     let cuadroResaltado;
+//     const tiempo = 1000
+
+//     for (const cuadro in cuadros) {
+//         if(numeroRandom === Number(cuadros[cuadro].innerText)) {
+//             cuadroResaltado = cuadros[cuadro]
+//             cuadroResaltado.style.opacity = 1
+//             rondaUsuario.push(cuadroResaltado)
+//             setTimeout(() => {
+//             cuadroResaltado.style.opacity = 0.5
+//             }, tiempo);
+//             }
+//     }
+//     console.log(rondaUsuario)
+//     return cuadroResaltado
+
     
-    console.log(numeroRandom)
+// }
 
+// function turnoJugador(cuadroResaltado) {
+
+//     resaltarCuadro(numeroRandom(), capturarCuadros())
+//     console.log(cuadroResaltado)
+
+    
+//     return
+// }
+
+// function numeroRandom() {
+
+//     const numRandom = (Math.floor(Math.random() * (4-1+1)+1))
+//     return numRandom
+// }
+
+// function capturarCuadros() {
+
+//     const amarillo = document.querySelector('#cuadro-uno')
+//     const rojo = document.querySelector('#cuadro-dos')
+//     const verde = document.querySelector('#cuadro-tres')
+//     const azul = document.querySelector('#cuadro-cuatro')
+
+
+
+//     let cuadros = {
+//         cuadroAmarillo : amarillo,
+//         cuadroRojo : rojo,
+//         cuadroVerde : verde,
+//         cuadroAzul : azul
+//     }
+//     return cuadros
+
+// }
+
+const uno = document.querySelector('#cuadro-uno')
+const dos = document.querySelector('#cuadro-dos')
+const tres = document.querySelector('#cuadro-tres')
+const cuatro = document.querySelector('#cuadro-cuatro')
+
+let arrayRonda = []
+let rondas = 0
+
+function obtenerCuadroRandom(random, cuadros) {
+    
     for (const cuadro in cuadros) {
-        if(numeroRandom === Number(cuadros[cuadro].innerText)) {
-            console.log(cuadros[cuadro])
-            cuadros[cuadro].style.opacity = 1
+        
+        const elemento = cuadros[cuadro]
+        if(Number(elemento.innerText) === random) {
+            arrayRonda.push(elemento)
         }
+
     }
+    console.log(arrayRonda)
+    return arrayRonda
+}
+obtenerCuadroRandom(calcRandom(), capturarCuadros())
+
+
+
+function capturarCuadros() {
     
+    let cuadros = {
+        cuadroUno : uno,
+        cuadroDos : dos,
+        cuadroTres : tres,
+        cuadroCuatro : cuatro
+    }
+
+    return cuadros
 }
 
-function numeroRandom() {
+function calcRandom() {
 
     const numRandom = (Math.floor(Math.random() * (4-1+1)+1))
     return numRandom
 }
-
-function capturarCuadros() {
-
-    const amarillo = document.querySelector('#cuadro-uno')
-    const rojo = document.querySelector('#cuadro-dos')
-    const verde = document.querySelector('#cuadro-tres')
-    const azul = document.querySelector('#cuadro-cuatro')
-
-
-
-    let cuadros = {
-        cuadroAmarillo : amarillo,
-        cuadroRojo : rojo,
-        cuadroVerde : verde,
-        cuadroAzul : azul
-    }
-    return cuadros
-
-}
-
-
 
 
 
